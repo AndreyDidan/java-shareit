7b4a66d86f0db3a18072dbe17ae1b713e5e11593
Andrey Didan <andrey_didan@mail.ru>
2024-12-04 15:00:25 +0300

    Направляю на проверку ТЗ 14

--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -1,12 +1,17 @@
 package ru.practicum.shareit.user.repository;
 
 import ru.practicum.shareit.user.User;
+
 import java.util.Optional;
 
 public interface UserRepository {
     Optional<User> findById(Long id);
+
     Optional<User> findByEmail(String email);
+
     User create(User user);
+
     User update(Long id, User user);
+
     void delete(Long id);
 }
--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -5,7 +5,10 @@ import ru.practicum.shareit.user.dto.UserDto;
 
 public interface UserService {
     UserDto findByIdUser(Long id);
+
     UserDto createUser(CreateUserDto createUserDto);
+
     UserDto updateUser(Long id, UserDto userDto);
+
     void deleteUser(Long id);
 }
